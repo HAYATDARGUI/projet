4cce44cb6913e7a5ffbf7a88d0aa85d2ef2e20c6
hayatdargui <hayatdargui72@gmail.com>
2022-09-14 17:38:00 +0100

    hyt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
 
 function App() {
+  let n=19;
+  let m=18;
+  let obj={nom:"hayat",class:"dev201",N :"9"}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hayat</h1>
+      <p>age = {n}</p> 
+      
+      <p>les information = {obj.nom}--{obj.class}--{obj.N}</p>
+
     </div>
   );
 }
